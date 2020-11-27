9c83709645c0b59c13b7926f758531550f4cbed8
ubuntu <ubuntu@localhost.localdomain>
2020-11-28 00:18:45 +0800

    add plugin helmdiff

--- helm.sh
+++ helm.sh
@@ -2,13 +2,13 @@
 set -e
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
+source ~/proxy.sh # use bash please
 if [ ! -f "/usr/local/bin/helm" ]; then
     echo "getting helm"
     curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 fi
 
 helmfile_install(){
-    source ~/proxy.sh # use bash please
     bin_url=https://github.com/roboll/helmfile/releases/download/v0.135.0/helmfile_linux_amd64
     [ ! -f helmfile_linux_amd64 ] && wget $bin_url -O helmfile_linux_amd64 && chmod +x helmfile_linux_amd64
     sudo mv helmfile_linux_amd64 /usr/local/bin/helmfile # precondition: if [ ! -f "/usr/local/bin/helmfile" ]; then
@@ -17,3 +17,5 @@ if [ ! -f "/usr/local/bin/helmfile" ]; then
     echo "getting helmfile"
     helmfile_install
 fi
+
+helm plugin install https://github.com/databus23/helm-diff
