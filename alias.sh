e9a3761a5ea5ce25a5fe1fd9a066511523a0fece
ubuntu <ubuntu@localhost.localdomain>
2020-12-23 18:15:39 +0800

    fix a alias bug

--- alias.sh
+++ alias.sh
@@ -2,7 +2,7 @@
 set -e
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
-alias gc=git clone
+alias gc='git clone'
 clashdir=/home/ubuntu/clash
 [ -f "$clashdir/clash" ] && alias clash="$clashdir/clash -d $clashdir"
 
